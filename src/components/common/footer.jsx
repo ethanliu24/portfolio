2ca1c38ea5b48708b1a732d64e30d94b3b5ac979
ethanliu24 <liu.ethan2005@gmail.com>
2024-10-28 01:41:41 -0400

    added footer

---
+++ src/components/common/footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <div id="footer">
+      <p>Made by Ethan Liu. Note: this is still under development :)</p>
+      <p>Copyright © Ethan Liu 2024 - All rights reserved</p>
+    </div>
+  )
+}
+
+export default Footer 
