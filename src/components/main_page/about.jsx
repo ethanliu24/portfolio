656ae63f7ca74ea023510e47a0dc2cd40eae53b1
ethanliu24 <liu.ethan2005@gmail.com>
2024-10-28 02:34:12 -0400

    added more styles and change contents to about page

--- src/components/main_page/about.jsx
+++ src/components/main_page/about.jsx
@@ -3,17 +3,20 @@ function About() {
     <div id="about" className="apply-spacing .txt-align-center">
       <h1 className="section-title">About</h1>
       <div className="contents">
-        <img src="src/assets/my_pic.jpeg" alt="my picture" className="picture" />
+        <img src="src/assets/cat.jpeg" alt="a cute cat" className="picture" />
         <div className="text-contents">
           <p>
-            Hello! I am a second-year Computer Science student at the University of Toronto with a passion 
-            for building web applications that come to life on both front and back ends.
-            While I love full-stack development, I am excited to dive into general software engineering, 
-            computer networks, AI, machine learning, operating systems and more!
+            Hello! I am a sophomore student at the University of Toronto studying Computer Science.
+            I have a huge passion for building softwares that come to life on both front and back ends.
+            While I love and the have most experience developing full-stack web applications, I am also 
+            excited to try out different fields including more general software engineering, computer 
+            networks, AI, machine learning, operating systems and more!
           </p>
           <p>
-            Outside school, I also do som fencing (the sport, not the ones in the garden) and am a 
-            music enthusiast. You can learn more about me <a href="">here</a>.
+            Outside school, I do fencing and I would argue that I'm the biggest music nerd among my friends. 
+            Although the cat in the image does not belong to me, he is one of the cutest cats I've met 
+            (I adore cats, they make funny faces and I put them in my presentations). 
+            You can learn more about me <a href="">here</a>.
           </p>
         </div>
       </div>
