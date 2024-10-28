663929fbcf2894293a48d18813121e50eae1aead
ethanliu24 <liu.ethan2005@gmail.com>
2024-10-27 20:31:25 -0400

    made and style nav bar

---
+++ src/components/common/NavBar.jsx
@@ -0,0 +1,25 @@
+
+// TODO fill in href
+function NavBar() {
+  return (
+    <nav className="nav-bar">
+      <div className="left">
+        <ul>
+          <li><a href="">Home</a></li>
+        </ul>
+      </div>
+
+      <div className="right">
+        <ul>
+          <li><a href="">About</a></li>
+          <li><a href="">Experiences</a></li>
+          <li><a href="">Projects</a></li>
+          <li><a href="">Contact</a></li>
+          <li><a href="">Playground</a></li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
