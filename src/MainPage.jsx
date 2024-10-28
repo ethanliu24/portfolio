fc22385ca8505e2353026be6d201e0c33a3c53b4
ethanliu24 <liu.ethan2005@gmail.com>
2024-10-28 00:51:36 -0400

    made and style about page

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,12 +1,13 @@
 import NavBar from './components/common/NavBar.jsx'
 import Header from './components/main_page/Header.jsx'
-
+import About from './components/main_page/About.jsx'
 
 function MainPage() {
   return (
     <>
       <NavBar />
       <Header />
+      <About /> 
     </>
   )
 }
---
+++ src/components/main_page/about.jsx
@@ -0,0 +1,24 @@
+function About() {
+  return (
+    <div id="about" className="apply-spacing">
+      <h1 className="section-title">About</h1>
+      <div className="contents">
+        <img src="src/assets/my_pic.jpeg" alt="my picture" className="picture" />
+        <div className="text-contents">
+          <p>
+            Hello! I am a second-year Computer Science student at the University of Toronto with a passion 
+            for building web applications that come to life on both front and back ends.
+            While I love full-stack development, I am excited to dive into general software engineering, 
+            computer networks, AI, machine learning, operating systems and more!
+          </p>
+          <p>
+            Outside school, I also do som fencing (the sport, not the ones in the garden) and am a 
+            music enthusiast. You can learn more about me <a href="">here</a>.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
