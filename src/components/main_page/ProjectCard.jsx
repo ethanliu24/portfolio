d75c5f4a120c84aec9dad9d3078932254fc72d7b
ethanliu24 <liu.ethan2005@gmail.com>
2024-10-31 15:39:39 -0400

    styled and added sample functionalities for projects

--- src/components/main_page/ProjectCard.jsx
+++ src/components/main_page/ProjectCard.jsx
@@ -1,18 +1,22 @@
 function ProjectCard({ name, description, tech, imgPath, githubLink, webLink, projectPagePath }) {
   return (
     <div className="project-card-container">
-      <div className="visiual-container">
-        <img src={imgPath} alt="image of the project" />
-        <div className="project-stacks">{tech.map((technology) => {
-          <div className="project-tech">{technology}</div>
-        })}</div>
-        <p className="project-description">{description}</p>
-      </div>
+      <a href={projectPagePath}>
+        <div className="visual-container">
+          <img src={imgPath} alt="image of the project" />
+          <div className="project-stack">{tech.map((technology, i) => {
+            return <div className="project-tech" key={`${name.toLowerCase()}-tech-${i}`}>{technology}</div>
+          })}</div>
+          <p className="project-description">{description}</p>
+        </div>
+      </a>
 
       <div className="title-container">
         <a href={projectPagePath}><h2 className="project-name">{name}</h2></a>
-        { githubLink ? <a href={githubLink} target="_blank" rel="noopener noreferrer"><img className="github-icon"  src="src/assets/icons/github.svg" /></a> : <></>}
-        { webLink ? <a href={webLink} target="_blank" rel="noopener noreferrer"><img className="web-icon" src="src/assets/icons/web.svg" /></a> : <></>}
+        <div className="icon-container">
+          { githubLink ? <a href={githubLink} target="_blank" rel="noopener noreferrer"><img className="github-icon icon" src="src/assets/icons/github.svg" /></a> : <></>}
+          { webLink ? <a href={webLink} target="_blank" rel="noopener noreferrer"><img className="web-icon icon" src="src/assets/icons/web.svg" /></a> : <></>}
+          </div>
       </div>
     </div>
   )
--- src/components/main_page/ProjectSect.jsx
+++ src/components/main_page/ProjectSect.jsx
@@ -6,13 +6,24 @@ function ProjectSect() {
     <div className="container">
       <h1 className="section-title">Projects</h1>
       {/* TODO fill in the link to projects page */}
-      <a id="view-more-projects" href="">view more »</a>
+      <a className="view-more" href="">view more »</a>
     </div>
     <div className="contents">
-      <ProjectCard 
+      <ProjectCard id="project-1"
         name="test" 
         description="sampel description" 
         tech={["tech 1", "tech 2"]}  
+        imgPath="src/assets/header_img.jpeg"
+        githubLink="https://github.com/ethanliu24/portfolio"
+        webLink="https://www.youtube.com/watch?v=KRtvCCDbLgQ"
+        projectPagePath="https://github.com/ethanliu24/portfolio"
+      />
+
+      <ProjectCard id="project-2"
+        name="test" 
+        description="sampel description" 
+        tech={["tech 1", "tech 2"]}  
+        imgPath="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqHbrPdXaablps22kGiPEGZRFLQr1ECspWDA&s"
         githubLink="https://github.com/ethanliu24/portfolio"
         webLink="https://www.youtube.com/watch?v=KRtvCCDbLgQ"
         projectPagePath="https://github.com/ethanliu24/portfolio"
