d535a39a6e09e2c625c617400a5cacb052f2072a
ethanliu24 <liu.ethan2005@gmail.com>
2024-10-31 02:19:24 -0400

    added tech stack component to main page

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -3,6 +3,7 @@ import Header from './components/main_page/Header.jsx'
 import About from './components/main_page/About.jsx'
 import Footer from './components/common/Footer.jsx'
 import InfoBar from './components/main_page/InfoBar.jsx'
+import TechStack from './components/main_page/TechStack.jsx'
 
 function MainPage() {
   return (
@@ -11,6 +12,7 @@ function MainPage() {
       <Header />
       <InfoBar />
       <About /> 
+      <TechStack />
       <Footer />
     </>
   )
