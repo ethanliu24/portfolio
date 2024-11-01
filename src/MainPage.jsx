5b3f6433c4a33f8aa788e9e2f85b88381e56452c
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-01 18:34:21 -0400

    changed header style and content

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -7,6 +7,9 @@ import TechStack from './components/main_page/TechStack.jsx'
 import Experiences from './components/main_page/Experiences.jsx'
 import ProjectSect from './components/main_page/ProjectSect.jsx'
 import Contact from './components/common/Contact.jsx'
+import gsp from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { ScrollTrigger } from 'gsap/all'
 
 function MainPage() {
   return (
--- src/components/main_page/Header.jsx
+++ src/components/main_page/Header.jsx
@@ -1,8 +1,9 @@
 function Header() {
   return (
     <div id="header">
-      <h1 className="text-shadow">Hey, I'm Ethan!</h1>
-      <h6 className="text-shadow">I'm a Software Developer</h6>
+      <p className="text-shadow">Hey! My name is</p>
+      <h1 className="text-shadow">Ethan Liu</h1>
+      <h6 className="text-shadow">I'm a software developer based in Toronto, specializing in full-stack web development.</h6>
     </div>
   )
 }
