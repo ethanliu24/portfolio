cf97ad85646c2e81ae70d89193dca1495efd8cc1
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-01 19:45:10 -0400

    added animation for nav bar

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -7,11 +7,26 @@ import TechStack from './components/main_page/TechStack.jsx'
 import Experiences from './components/main_page/Experiences.jsx'
 import ProjectSect from './components/main_page/ProjectSect.jsx'
 import Contact from './components/common/Contact.jsx'
-import gsp from 'gsap'
+import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 import { ScrollTrigger } from 'gsap/all'
 
 function MainPage() {
+  gsap.registerPlugin(useGSAP)
+  gsap.registerPlugin(ScrollTrigger)
+
+  useGSAP(() => {
+    let navBarAnimation = gsap.timeline();
+
+    navBarAnimation.from("#nav-bar-home", { opacity: 0, duration: 2 }, 0)
+    navBarAnimation.from("#nav-bar-about", { opacity: 0, duration: 2 }, 0.4)
+    navBarAnimation.from("#nav-bar-experiences", { opacity: 0, duration: 2 }, 0.6)
+    navBarAnimation.from("#nav-bar-projects", { opacity: 0, duration: 2 }, 0.8)
+    navBarAnimation.from("#nav-bar-contacts", { opacity: 0, duration: 2 }, 1)
+    navBarAnimation.from("#nav-bar-playground", { opacity: 0, duration: 2 }, 1.2)
+
+  })
+
   return (
     <>
       <NavBar />
