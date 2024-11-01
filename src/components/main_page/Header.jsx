c891162bff72b01f02e2ace66448ba3421770d2d
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-01 18:34:40 -0400

    changed header content

--- src/components/main_page/Header.jsx
+++ src/components/main_page/Header.jsx
@@ -4,6 +4,9 @@ function Header() {
       <p className="text-shadow">Hey! My name is</p>
       <h1 className="text-shadow">Ethan Liu</h1>
       <h6 className="text-shadow">I'm a software developer based in Toronto, specializing in full-stack web development.</h6>
+      <div id="mouse-container">
+        <div id="mouse-scroll"></div>
+      </div>
     </div>
   )
 }
