016a4ab62bdf198493fae576cffa489cf9895bbf
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-02 00:37:16 -0400

    changed header and added star animations using paper js

--- src/components/main_page/Header.jsx
+++ src/components/main_page/Header.jsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react"
+import { initStars } from "../../../starAnimation.js"
+
 function Header() {
+  const radiusRange = [0.1, 0.5]
+  const velocityRange = [0.1, 0.5]
+
+
+  useEffect(() => {
+    const canvas = document.getElementById("star-canvas");
+    initStars(canvas, radiusRange, velocityRange)
+  }, [])
+
   return (
     <div id="header">
+      <canvas id="star-canvas" resize="true"></canvas>
       <p className="text-shadow">Hey! My name is</p>
       <h1 className="text-shadow">Ethan Liu</h1>
       <h6 className="text-shadow">I'm a software developer based in Toronto, specializing in full-stack web development.</h6>
