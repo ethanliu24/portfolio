ce4c559229f95d6e6f7c333b2cb54058fb576801
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-04 19:44:08 -0500

    changed footer content

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <div id="footer">
-      <p>Made by Ethan Liu. Note: this is still under development :)</p>
+      <p>Still under development :)</p>
       <p>Copyright © Ethan Liu 2024 - All rights reserved</p>
     </div>
   )
