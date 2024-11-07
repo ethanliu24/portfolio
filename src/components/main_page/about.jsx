51898d076bc7a6b8481d44a818f971a314647f13
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-06 21:23:31 -0500

    changed about section contents

--- src/components/main_page/about.jsx
+++ src/components/main_page/about.jsx
@@ -6,15 +6,22 @@ function About() {
         <img src="src/assets/cat.jpeg" alt="a cute cat" className="picture" />
         <div className="text-contents">
           <p>
-            Hello! I am a sophomore student at the University of Toronto studying Computer Science.
-            I have a huge passion for building softwares that come to life on both front and back ends.
-            While I love and have the most experience developing full-stack web applications, I am also 
-            excited to try out different fields including more general software engineering, computer 
-            networks, AI, machine learning, operating systems and more!
-          </p>
+            Hello! I'm a second-year student studying computer science at the University of Toronto. 
+            My main experience roots in web development with a slight emphasis on the front-end currently,
+            using modern technologies such as React and other front-end libraries. 
+          </p> 
+
           <p>
-            Outside school, I do fencing and I would argue that I'm the biggest music nerd among my friends. 
-            Although the cat in the image does not belong to me, he is one of the cutest cats I've met 
+            I love challenging myself with creative problem-solving and exploring new 
+            frameworks/libraries to use in my projects.
+            While I absolutely enjoy creating meaning full stack web applications, 
+            I am equally as excited to explore other fields in computer science, including 
+            computer networks, AI, ML, UI/UX, computer graphics, cybersecurity and more!
+          </p>
+
+          <p>
+            Outside school, I do fencing and I claim to be the biggest music nerd among my friends. 
+            Although the cat in the image does not belong to me, he is my favourite meatball. 
             (I adore cats, they make funny faces and I put them in my presentations). 
             You can learn more about me <a href="">here</a>.
           </p>
