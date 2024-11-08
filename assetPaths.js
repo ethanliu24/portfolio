7565d9ae748037b4d0f1a61d8d0cd166481e2dd3
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-07 23:44:28 -0500

    added global paths for assets

---
+++ assetPaths.js
@@ -0,0 +1,37 @@
+const rootPath = "src/assets/"
+const iconPath = "icons/"
+const projectsPath = "projects/"
+
+const paths = {
+  // general
+  aboutImg: rootPath + "cat.jpeg",
+  underDevelopment: rootPath + "under_development.gif",
+  resume: rootPath + "Master_Resume.pdf",
+  
+  // icons
+  downloadIcon: rootPath + iconPath + "download.svg",
+  externalIcon: rootPath + iconPath + "external.svg",
+  githubIcon: rootPath + iconPath + "github.svg",
+  instagramIcon: rootPath + iconPath + "instagram.svg",
+  linkedInIcon: rootPath + iconPath + "linkedin.svg",
+  mailIcon: rootPath + iconPath + "mail.svg",
+  menuIcon: rootPath + iconPath + "root.svg",
+  pinIcon: rootPath + iconPath + "pin.svg",
+  webIcon: rootPath + iconPath + "web.svg",
+
+  // dev tool path
+  devToolPath: rootPath + "dev_tools/",
+
+  // projects
+  courseograhpy: rootPath + projectsPath + "courseography.png",
+  commingSoon: rootPath + projectsPath + "comming_soon.png",
+
+  // dementia-bot
+  dementiaBotBanner: rootPath + projectsPath + "dementia-bot/banner.png",
+
+  // jae-market
+  JAEMarketBanner: rootPath + projectsPath + "jae-market/banner.png",
+
+  // portfolio
+  portfolioBanner: rootPath + projectsPath + "portfolio/banner.png",
+}
