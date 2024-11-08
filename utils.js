fc6213dd9a83629a55df5e090f3bebef89912c6e
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-07 23:18:45 -0500

    removed useless files

--- utils.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", () => {
-  const navBar = document.querySelector(".nav-bar");
-  if (window.scrollY > 10) {
-    navBar.classList.add("not-on-top");
-  } else {
-    navBar.classList.remove("not-on-top");
-  }
-});
