a15fcc97ebcbfa4ca61f65645298b23b9030b19e
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-09 00:22:50 -0500

    nav bar href

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -4,20 +4,18 @@ import assetPaths from '../../../assetPaths.js'
 function NavBar() {
   const [open, setOpen] = useState(true)
 
-  const idUrl = "https://ethanliu24.github.io/portfolio/"
-
   return (
     <nav id="nav-bar">
       <ul id="home">
-        <li id="nav-bar-home"><a href={idUrl}>Home</a></li>
+        <li id="nav-bar-home"><a href={"#header"}>Home</a></li>
       </ul>
 
       <ul id="links">
-        <li><a href={idUrl + "#about"}>About</a></li>
-        <li><a href={idUrl + "#experiences"}>Experiences</a></li>
+        <li><a href={"#about"}>About</a></li>
+        <li><a href={"#experiences"}>Experiences</a></li>
         {/* TODO change projects to go to /projects route */}
-        <li><a href={idUrl + "#project-sect"}>Projects</a></li> 
-        <li><a href={idUrl + "#contact"}>Contacts</a></li>
+        <li><a href={"#project-sect"}>Projects</a></li> 
+        <li><a href={"#contact"}>Contacts</a></li>
         <li><a href="">More</a></li>
       </ul>
       
