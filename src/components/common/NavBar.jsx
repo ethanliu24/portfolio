2bae754fc749ac0dfc15e07f8e6f7fc12e6a9aad
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-09 00:36:03 -0500

    fucking routes

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -14,8 +14,8 @@ function NavBar() {
         <li><a href="#/#about">About</a></li>
         <li><a href="#/#experiences">Experiences</a></li>
         {/* TODO change projects to go to /projects route */}
-        <li><a href="#project-sect">Projects</a></li> 
-        <li><a href="#contact">Contacts</a></li>
+        <li><a href="#/#project-sect">Projects</a></li> 
+        <li><a href="#/#contact">Contacts</a></li>
         <li><a href="">More</a></li>
       </ul>
       
