c20586fa134a9b6e36b6c1c6df13c54c6d4b209f
ethanliu24 <liu.ethan2005@gmail.com>
2024-11-08 23:00:37 -0500

    changed BrowserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react'
+import { Routes, Route, HashRouter } from 'react-router-dom';
 
 const Home = React.lazy(() => import('./MainPage.jsx'))
 const Projects = React.lazy(() => import('./ProjectsPage.jsx'))
@@ -7,15 +7,13 @@ const UnderDevelopement = React.lazy(() => import('./UnderDevelopment.jsx'))
 
 function App() {
   return (
-    <Router>
-      <Suspense>
-        <Routes>
-          <Route path="portfolio/" element={<Home />} />
-          <Route path="portfolio/projects" element={<UnderDevelopement />} />
-          <Route path="portfolio/under-development" element={<UnderDevelopement />}/>
-        </Routes>
-      </Suspense>
-    </Router>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<UnderDevelopement />} />
+        <Route path="/under-development" element={<UnderDevelopement />}/>
+      </Routes>
+    </HashRouter>
   )
 }
 
